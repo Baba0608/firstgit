c85bdb997bba428e13433a6bf944fa290de37636
Baba0608 <babafakruddinnitturu@gmail.com>
2023-09-07 20:54:30 +0530

    Odd list items are styled

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 
-console.log("I love js");
+const title = document.getElementById("Title");
+
+title.innerHTML = "My Title";
+
+
+const header = document.getElementById("header");
+
+header.style.border = "2px solid red";
+
+
+const Listitem = document.querySelector("#ListItem");
+
+Listitem.children[0].style.color = "red";
+// Listitem.children[2].style.backgroundColor = "green";
+
+Listitem.style.fontWeight = "bold";
+
+const item = document.querySelectorAll(".Litem");
+
+item[1].style.backgroundColor = "green";
+item[2].style.display = "none";
+
+const fruit = document.querySelectorAll("li:nth-child(odd)");
+
+for (let i = 0; i < fruit.length;i++){
+    fruit[i].style.color = "orange";
+}
+
