5cfda03369bc47dec6f41181db2f00237368578b
Baba0608 <babafakruddinnitturu@gmail.com>
2023-09-23 10:57:25 +0530

    Requests Functions are completed

--- GET , POST , DELETE , PUT Requests/requests.js
+++ GET , POST , DELETE , PUT Requests/requests.js
@@ -1,49 +1,148 @@
 // GET REQUEST
 function getTodos() {
-  console.log("GET Request");
+  //   console.log("GET Request");
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos", {
+      params: { _limit: 5 },
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log("POST Request");
+  //   console.log("POST Request");
+  axios
+    .post("https://jsonplaceholder.typicode.com/todos", {
+      title: "New data",
+      completed: false,
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log("PUT/PATCH Request");
+  //   console.log("PUT/PATCH Request");
+  axios
+    .patch("https://jsonplaceholder.typicode.com/todos/1", {
+      title: "Updated data",
+      completed: true,
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log("DELETE Request");
+  //   console.log("DELETE Request");
+  axios
+    .delete("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
   console.log("Simultaneous Request");
+  axios
+    .all([
+      axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5"),
+      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5"),
+    ])
+    .then(axios.spread((todos, posts) => showOutput(posts)))
+    .catch((err) => console.error(err));
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log("Custom Headers");
+  //   console.log("Custom Headers");
+  const data = {
+    title: "new title",
+    completed: true,
+  };
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "sometoken",
+    },
+  };
+  axios
+    .post("https://jsonplaceholder.typicode.com/todos", data, config)
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log("Transform Response");
+  //   console.log("Transform Response");
+  const options = {
+    method: "post",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    data: {
+      title: "Hello World!",
+    },
+    transformResponse: axios.defaults.transformResponse.concat((data) => {
+      data.title = data.title.toUpperCase();
+      return data;
+    }),
+  };
+
+  axios(options).then((res) => showOutput(res));
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log("Error Handling");
+  //   console.log("Error Handling");
+  axios
+    .get("https://jsonplaceholder.typicode.com/todoss")
+    .then((res) => showOutput(res))
+    .catch((err) => {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      }
+    });
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log("Cancel Token");
+  //   console.log("Cancel Token");
+  const source = axios.CancelToken.source();
+
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos", {
+      cancelToken: source.token,
+    })
+    .then((res) => showOutput(res))
+    .catch((thrown) => {
+      if (axios.isCancel(thrown)) {
+        console.log("Request cancled", thrown.message);
+      }
+    });
+
+  if (true) {
+    source.cancel("Request cancled");
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(
+  (config) => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
+
+    return config;
+  },
+  (error) => {
+    return new Promise.reject(error);
+  }
+);
 
 // AXIOS INSTANCES
 
