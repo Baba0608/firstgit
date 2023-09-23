730da84eb970fbcba8c56c332ba2fd379a3ca5a3
Baba0608 <babafakruddinnitturu@gmail.com>
2023-09-23 19:06:08 +0530

    Data retrived from crud and data display onrefresh

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const submit = document.getElementById("Submit");
 
 const list = document.getElementById("List");
 
-const listEl = document.getElementById("list");
-
 submit.addEventListener("click", addToCurdCurd);
 
+// ------------------------------------------------------------------------------------
+// adding event listener to submit button to sent data to crudcrud
 function addToCurdCurd(e) {
   e.preventDefault();
 
@@ -30,10 +30,24 @@ function addToCurdCurd(e) {
       .then((res) => {
         console.log(res.data);
         const li = document.createElement("li");
+        const span = document.createElement("span");
         const para = document.createElement("p");
+        const deleteBtn = document.createElement("button");
+        const editBtn = document.createElement("button");
+
+        deleteBtn.className = "deleteBtn";
+        editBtn.className = "edit";
+
+        deleteBtn.textContent = "X";
+        editBtn.textContent = "Edit";
+
+        span.textContent = res.data._id;
         para.innerHTML =
           res.data.username + " - " + res.data.mail + " - " + res.data.mobile;
+        li.appendChild(span);
         li.appendChild(para);
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
         list.appendChild(li);
       })
       .catch((err) => console.error(err));
@@ -46,6 +60,46 @@ function addToCurdCurd(e) {
   }
 }
 
+// -------------------------------------------------------------------------------------
+
+// displaying the data on webpage which is in crudcrud
+function display() {
+  axios
+    .get(
+      "https://crudcrud.com/api/523cc2a67c7448ec86e591d6bda586f9/appointmentData"
+    )
+    .then((res) => {
+      res.data.forEach((objEl) => {
+        const span = document.createElement("span");
+        const li = document.createElement("li");
+        const para = document.createElement("p");
+        const deleteBtn = document.createElement("button");
+
+        const editBtn = document.createElement("button");
+
+        deleteBtn.className = "deleteBtn";
+        editBtn.className = "edit";
+        editBtn.textContent = "Edit";
+
+        deleteBtn.textContent = "X";
+
+        span.textContent = objEl._id;
+
+        para.textContent =
+          objEl.username + " - " + objEl.mail + " - " + objEl.mobile;
+
+        li.appendChild(span);
+        li.appendChild(para);
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+
+        list.appendChild(li);
+      });
+    });
+}
+
+display();
+// -------------------------------------------------------------------------------------
 function emptyData(arr) {
   for (let i = 0; i < arr.length; i++) {
     axios
@@ -57,6 +111,32 @@ function emptyData(arr) {
   }
 }
 
-arr = ["650ecc27b987ad03e876bb9a"];
+arr = [""];
 
 // emptyData(arr);
+
+// ------------------------------------------------------------------------------------------
+
+// adding event to delete data from webpage as well as from crudcrud
+list.addEventListener("click", deleteFromCrud);
+
+function deleteFromCrud(e) {
+  if (e.target.classList.contains("deleteBtn")) {
+    const id = e.target.parentElement.firstChild.textContent;
+    // console.log(id);
+
+    const ele = e.target.parentElement;
+    // console.log(ele);
+
+    list.removeChild(ele);
+
+    axios
+      .delete(
+        `https://crudcrud.com/api/523cc2a67c7448ec86e591d6bda586f9/appointmentData/${id}`
+      )
+      .then((res) => console.log("deleted"))
+      .catch((err) => console.error(err));
+  }
+}
+
+// -----------------------------------------------------------------------------------------------
